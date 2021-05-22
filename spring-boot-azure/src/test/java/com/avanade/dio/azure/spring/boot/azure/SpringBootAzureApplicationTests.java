697b096322e9fc2ab4d56398dcce7424a61ec478
annezdz <annezdz@hotmail.com>
2021-05-22 10:21:52 -0300

    initial commit

---
+++ spring-boot-azure/src/test/java/com/avanade/dio/azure/spring/boot/azure/SpringBootAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avanade.dio.azure.spring.boot.azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
